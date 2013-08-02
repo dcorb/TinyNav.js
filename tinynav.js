ce503e75a47b507a29057c06d9484a0d6a74b6a0
Corbacho <davidcorbacho@gmail.com>
2013-08-02 11:31:54 +0300

    Fix activeOption and better performance

--- tinynav.js
+++ tinynav.js
@@ -1,84 +1,84 @@
+/*! David Corbacho Fork v.1.1.1 https://github.com/dcorb/TinyNav.js from http://tinynav.viljamis.com v1.1  */
 /*! http://tinynav.viljamis.com v1.1 by @viljamis */
 (function ($, window, i) {
-  $.fn.tinyNav = function (options) {
+    $.fn.tinyNav = function (options) {
 
-    // Default settings
-    var settings = $.extend({
-      'active' : 'selected', // String: Set the "active" class
-      'header' : '', // String: Specify text for "header" and show header instead of the active item
-      'label'  : '' // String: sets the <label> text for the <select> (if not set, no label will be added)
-    }, options);
+        // Default settings
+        var settings = $.extend({
+            'active' : 'active', // String: Set the "active" class
+            'header' : '', // String: Specify text for "header" and show header instead of the active item
+            'label'  : '' // String: sets the <label> text for the <select> (if not set, no label will be added)
+        }, options);
 
-    return this.each(function () {
+        return this.each(function () {
 
-      // Used for namespacing
-      i++;
+            // Used for namespacing
+            i++;
 
-      var $nav = $(this),
-        // Namespacing
-        namespace = 'tinynav',
-        namespace_i = namespace + i,
-        l_namespace_i = '.l_' + namespace_i,
-        $select = $('<select/>').attr("id", namespace_i).addClass(namespace + ' ' + namespace_i);
+            var $nav = $(this),
+            // Namespacing
+                namespace = 'tinynav',
+                namespace_i = namespace + i,
+                l_namespace_i = '.l_' + namespace_i,
+                $select = $('<select/>').attr("id", namespace_i).addClass(namespace + ' ' + namespace_i);
 
-      if ($nav.is('ul,ol')) {
-
-        var activeOptionIndex = $(l_namespace_i + ' li')
-            .index($(l_namespace_i + ' li.' + settings.active));
-            
-        
-        if (settings.header !== '') {
-          if (!activeOptionIndex == -1) {
-            $select.append($('<option/>').text(settings.header));
-            activeOptionIndex = 0;
-          }
-        }
-
-        // Build options
-        var options = '';
-
-        $nav
-          .addClass('l_' + namespace_i)
-          .find('a')
-          .each(function () {
-            options += '<option value="' + $(this).attr('href') + '">';
-            var j;
-            for (j = 0; j < $(this).parents('ul, ol').length - 1; j++) {
-              options += '- ';
+            if (!$nav.is('ul,ol')) {
+                return;
             }
-            options += $(this).text() + '</option>';
-          });
+            // Build options
+            var options = '';
 
-        // Append options into a select
-        $select.append(options);
+            $nav
+                .addClass('l_' + namespace_i)
+                .find('a')
+                .each(function () {
+                    options += '<option value="' + $(this).attr('href') + '">';
+                    var j;
+                    for (j = 0; j < $(this).parents('ul, ol').length - 1; j++) {
+                        options += '- ';
+                    }
+                    options += $(this).text() + '</option>';
+                });
 
-        // Select the active item
-        if (activeOptionIndex) {
-          $select.find(':eq(' + activeOptionIndex + ')')
-            .attr('selected', true);
-        }
 
-        // Change window location
-        $select.change(function () {
-          window.location.href = $(this).val();
+            var activeOptionIndex = $nav.find('li').index($nav.find('li.' + settings.active));
+
+            if (settings.header !== '') {
+                if (activeOptionIndex === -1) {
+                    $select.append($('<option/>').text(settings.header));
+                    activeOptionIndex = 0;
+                }
+            }
+
+            // Append options into a select
+            $select.append(options);
+
+
+            // Select the active item
+            if (activeOptionIndex) {
+                $select.find(':eq(' + activeOptionIndex + ')')
+                    .attr('selected', true);
+            }
+
+            // Change window location
+            $select.change(function () {
+                window.location.href = $(this).val();
+            });
+
+            // Inject select
+            $(l_namespace_i).after($select);
+
+            // Inject label
+            if (settings.label) {
+                $select.before(
+                    $("<label/>")
+                        .attr("for", namespace_i)
+                        .addClass(namespace + '_label ' + namespace_i + '_label')
+                        .append(settings.label)
+                );
+            }
+
         });
 
-        // Inject select
-        $(l_namespace_i).after($select);
-
-        // Inject label
-        if (settings.label) {
-          $select.before(
-            $("<label/>")
-              .attr("for", namespace_i)
-              .addClass(namespace + '_label ' + namespace_i + '_label')
-              .append(settings.label)
-          );
-        }
-
-      }
-
-    });
-
-  };
+    };
 })(jQuery, this, 0);
